a4cc0d4c3c3153c3d1c02efaf6fa7594b0ed287f
Nir <nirkoren1@gmail.com>
2021-10-16 17:51:46 +0300

    .

--- agent.py
+++ agent.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 import tensorflow.keras as keras
 from keras.optimizers import Adam
 import numpy as np
+import pickle
+import os
 
 
 class Agent:
@@ -39,15 +41,21 @@ class Agent:
         self.target_critic1.compile(optimizer=Adam(learning_rate=beta), loss='mean_squared_error')
         self.target_critic2.compile(optimizer=Adam(learning_rate=beta), loss='mean_squared_error')
 
-    def take_an_action(self, observasion):
+    def take_an_action(self, observation):
         if self.step_cntr < self.warmup:
             a = np.random.normal(scale=self.noise, size=(self.n_actions,))
         else:
-            state = tf.convert_to_tensor([observasion], dtype=tf.float32)
+            state = tf.convert_to_tensor([observation], dtype=tf.float32)
             a = self.actor.feed_forward(state)[0]
         a += np.random.normal(scale=self.noise)
         a = tf.clip_by_value(a, self.low_limit, self.high_limit)
         self.step_cntr += 1
+        return a
+
+    def take_an_action_for_real(self, observation):
+        state = tf.convert_to_tensor([observation], dtype=tf.float32)
+        a = self.actor.feed_forward(state)[0]
+        a = tf.clip_by_value(a, self.low_limit, self.high_limit)
         return a
 
     def remember(self, state, action, reward, state_, done):
@@ -85,16 +93,16 @@ class Agent:
             critic1_loss = tf.losses.MSE(target_q, q1)
             critic2_loss = tf.losses.MSE(target_q, q2)
 
-            # calculate the gradients of the two critics
-            critic1_gradient = tf.gradients(critic1_loss, self.critic1.trainable_weights)
-            critic2_gradient = tf.gradients(critic2_loss, self.critic2.trainable_weights)
+        # calculate the gradients of the two critics
+        critic1_gradient = tf.gradients(critic1_loss, self.critic1.trainable_weights)
+        critic2_gradient = tf.gradients(critic2_loss, self.critic2.trainable_weights)
 
-            self.critic1.optimizer.apply_gradients(zip(critic1_gradient, self.critic1.trainable_variables))
-            self.critic2.optimizer.apply_gradients(zip(critic2_gradient, self.critic2.trainable_variables))
+        self.critic1.optimizer.apply_gradients(zip(critic1_gradient, self.critic1.trainable_variables))
+        self.critic2.optimizer.apply_gradients(zip(critic2_gradient, self.critic2.trainable_variables))
 
-            self.learn_step_cntr += 1
+        self.learn_step_cntr += 1
 
-        if self.learn_step_cntr % 2 != 0:
+        if self.learn_step_cntr % self.update_actor_every != 0:
             return
         with tf.GradientTape() as tape:
             actions = self.actor.feed_forward(states)
@@ -103,4 +111,34 @@ class Agent:
 
             actor_gradient = tf.gradients(actor_loss, self.critic1.trainable_weights)
             self.actor.optimizer.apply_gradients(zip(actor_gradient, self.actor.trainable_variables))
+
+            self.update_nets_parameters()
+
+    def update_nets_parameters(self):
+        # update actor weights
+        weights = []
+        target_weights = self.targrt_actor.weights
+        for idx, weight in enumerate(self.actor.weights):
+            weights.append(weight * self.tau + (1 - self.tau) * target_weights[idx])
+        self.targrt_actor.set_weights(weights)
+
+        # update critic1 weights
+        weights = []
+        target_weights = self.target_critic1.weights
+        for idx, weight in enumerate(self.critic1.weights):
+            weights.append(weight * self.tau + (1 - self.tau) * target_weights[idx])
+        self.target_critic1.set_weights(weights)
+
+        # update critic2 weights
+        weights = []
+        target_weights = self.target_critic2.weights
+        for idx, weight in enumerate(self.critic2.weights):
+            weights.append(weight * self.tau + (1 - self.tau) * target_weights[idx])
+        self.target_critic2.set_weights(weights)
+
+    def save_agent(self, path, score):
+        files = os.listdir(path)
+        self.actor.save_weights(path)
+        # self.actor.save(path + f"/{len(files) + 1}-fitness=" + str(score)[:7])
+        print(f"Agent saved with {score} score")
 
---
+++ gym/walker_hard/gym_trainer.py
@@ -0,0 +1,52 @@
+import numpy as np
+import gym
+import sys
+import os
+import agent
+
+env = gym.make("BipedalWalkerHardcore-v3")
+ag = agent.Agent(alpha=0.001, beta=0.001, input_dims=env.observation_space.shape[0], n_actions=env.action_space.shape[0],
+                 env_high=env.action_space.high[0], env_low=env.action_space.low[0], tau=0.05, batch_size=100)
+score_history = []
+
+
+def cr_new_file():
+    dir_path = os.path.dirname(os.path.realpath(__file__)) + r'\agents'
+    dirs = os.listdir(dir_path)
+    if len(dirs) == 0:
+        os.mkdir(dir_path + r'\run-1')
+        return r'\run-1'
+    else:
+        os.mkdir(dir_path + rf'\run-{len(dirs) + 1}')
+        return rf'\run-{len(dirs) + 1}'
+
+
+if __name__ == '__main__':
+    # file_destination = cr_new_file()
+    loop = 0
+    while True:
+        loop += 1
+        sys.stdout.write(f"\r{loop}")
+        sys.stdout.flush()
+        observation = env.reset()
+        score = 0
+        while True:
+            action = ag.take_an_action(observation)
+            ob = observation
+            observation, reward, done, info = env.step(action)
+            ag.memory.save_step(ob, action, reward, observation, done)
+            score += reward
+            if done:
+                break
+        score_history.append(score)
+        if score == max(score_history):
+            print('')
+            ag.save_agent(r'C:\Users\Nirkoren\PycharmProjects\Td3\gym\walker_hard\agents\actor', score)
+        observation = env.reset()
+        if loop % 100 == 0:
+            while True:
+                env.render()
+                action = ag.take_an_action_for_real(observation)
+                observation, reward, done, info = env.step(action)
+                if done:
+                    break
